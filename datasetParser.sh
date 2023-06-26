0def8d3d9130b924feabe79994e36f821cb547df
mataktelis11 <mataktelis01@gmail.com>
2023-06-26 03:49:06 +0300

    dataset ok

---
+++ datasetParser.sh
@@ -0,0 +1,8 @@
+
+cp ../AudioMNIST/data/??/0*.wav audioData2/zero
+cp ../AudioMNIST/data/??/1*.wav audioData2/one
+cp ../AudioMNIST/data/??/2*.wav audioData2/two
+cp ../AudioMNIST/data/??/3*.wav audioData2/three
+cp ../AudioMNIST/data/??/4*.wav audioData2/four
+cp ../AudioMNIST/data/??/5*.wav audioData2/five
+
